d2231ce95493d8a22fa59c343bd7f53b2474faf9
Hugo <1161155@isep.ipp.pt>
2018-03-14 10:10:40 -0700

    Pipes Exercicio 1

---
+++ 2 - Pipes/ex01/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <wait.h>
+#include <time.h>
+
+int main(){
+	 
+	 pid_t temp;
+	 
+	 int fd[2];
+	 
+	 if(pipe(fd)<0){
+		 perror("Erro creating pipe");
+		 }
+	 
+	 pid_t p = fork();
+	 if( p < 0 ){
+		 perror("Error at the fork");
+		 }
+	 if(p != 0){
+		close(fd[0]);
+		temp = p;
+		 if(write(fd[1],&temp,sizeof(temp))<0){
+			 perror("Error with write");			 
+			 }
+		 printf("o meu id é %d\n",p);	 
+		 close(fd[1]);
+	 }else{
+		close(fd[1]);
+		if(read(fd[0],&temp,sizeof(temp)) < 0){
+			perror("Error with read");			
+			}
+		printf("o id do meu pai é %d\n",temp);
+		close(fd[0]);
+	 }
+	return 0;
+}
