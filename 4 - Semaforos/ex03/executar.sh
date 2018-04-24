33d1d653a8a1d2c69841873a797410db093231cb
Hugo <1161155@isep.ipp.pt>
2018-04-24 17:22:19 +0100

    Semaforos exercicio 3

---
+++ 4 - Semaforos/ex03/executar.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#type in terminal "bash executar.sh"
+for ((i = 1; i<=50;i++))
+
+do exec ./ex03 &
+
+done
---
+++ 4 - Semaforos/ex03/main.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <wait.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <string.h>
+#include <semaphore.h>
+
+//documento que mostra o raciocinio: algoritmo.txt
+
+void fail_sem(sem_t *sem){
+	if(sem==SEM_FAILED){
+			perror("sem_open falhou");
+		exit(1);
+	}
+}
+
+void fail_Sunlink(int i){
+	if(i==-1){
+			perror("sem unlink falhou");
+			exit(1);
+		}
+	}
+
+void fail_unmap(int r){
+	if(r == -1) {
+			perror("Unmap falhado");
+	}
+}
+
+void fail_close(int r){
+	if(r == -1) {
+			perror("Close falhado");
+	}
+}
+
+void fail_unlink(int r){
+	if(r == -1) {
+			perror("Shm unlink falhou");
+	}
+}
+
+typedef struct line
+{
+	char str[50][80];
+	int count;
+} line;
+
+int main(){
+	
+	sem_t *semaforo; //semaforo
+	line* ln; //apontador para a estrutura
+	int fd=0, size = sizeof(line); // memory sharing variables
+	char s[80]; //array para a mensagem
+	
+	//Concatenar a mensagem
+	sprintf(s,"I’m the Father - with  PID  %d",getpid());
+	
+	//memory sharing
+	fd = shm_open("/shm", O_CREAT|O_RDWR, S_IRUSR|S_IWUSR);
+	ftruncate(fd, size);
+	ln = (line*)mmap(NULL, size, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
+	
+	//open do semaforo
+	semaforo = sem_open("semaforo", O_CREAT, 0644, 1);
+	fail_sem(semaforo);
+	
+	sem_wait(semaforo);// muda o semaforo para 0 para que mais nenhum processo possa aceder
+
+	int num = ln->count; //obetem o numero de mensagens que ja foram introduzidas na matriz
+	
+	strcpy(ln->str[num],s); //introduz uma mensagem na matriz
+	
+	printf("%s\n",ln->str[num]); //da print ao que foi introduzido na matriz
+	
+	ln->count++;//incrementa o contador de mensagens
+	
+	sleep(1);//sleep
+	
+	sem_post(semaforo); // muda o semaforo para 1 indicando que outros processos vao poder mexer na memoria partilhada
+	
+	// este ciclo dá unlink ao semaforo e á memoria partilhada caso seja o ultimo dos processos
+	if(num == 49){
+		
+		fail_Sunlink(sem_unlink("semaforo"));//Unlinking of the semaphore
+
+		fail_unmap(munmap(ln,size)); //unmapping of the memory
+
+		fail_close(close(fd)); // closing of the memory
+
+		fail_unlink(shm_unlink("/shm")); // unlinking of the memory
+
+	}
+
+	return 0;
+}
+
